396a122720d2f8e143a47273b0f5bac810a5c4bc
ezbarretos <ezbarretos@gmail.com>
2017-04-13 22:18:22 -0500

    test report

--- omod/src/main/java/org/openmrs/module/lagtimereport/web/controller/LagTimeReportingController.java
+++ omod/src/main/java/org/openmrs/module/lagtimereport/web/controller/LagTimeReportingController.java
@@ -11,9 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Encounter;
+import org.openmrs.Form;
+import org.openmrs.api.EncounterService;
+import org.openmrs.module.lagtimereport.FormValue;
 import org.openmrs.module.lagtimereport.LagTimeReport;
 import org.openmrs.module.lagtimereport.LagTimeReportSetup;
-import org.openmrs.module.lagtimereport.propertyeditor.LagTimeReportEditor;
 import org.openmrs.module.lagtimereport.service.LagTimeReportService;
 import org.openmrs.module.lagtimereport.service.LagTimeReportSetupService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,36 +34,46 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class LagTimeReportingController {
-	
+
 	/** Logger for this class and subclasses */
 	protected final Log log = LogFactory.getLog(getClass());
-	
+
 	@Autowired
 	LagTimeReportService reportService;
-	
+
 	@Autowired
 	LagTimeReportSetupService lagtimeService;
-	
+
+	private EncounterService encounterService;
+
 	public LagTimeReportService getReportService() {
 		return reportService;
 	}
-	
+
 	public void setReportService(LagTimeReportService reportService) {
 		this.reportService = reportService;
 	}
-	
+
 	public LagTimeReportSetupService getLagtimeService() {
 		return lagtimeService;
 	}
-	
+
 	public void setLagtimeService(LagTimeReportSetupService lagtimeService) {
 		this.lagtimeService = lagtimeService;
 	}
-	
+
+	public EncounterService getEncounterService() {
+		return encounterService;
+	}
+
+	public void setEncounterService(EncounterService encounterService) {
+		this.encounterService = encounterService;
+	}
+
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.registerCustomEditor(LagTimeReport.class, new PropertyEditorSupport() {
-			
+
 			@Override
 			public void setAsText(String text) {
 				LagTimeReport lagtime = reportService.getLagTimeReport(Integer.parseInt(text));
@@ -68,38 +81,54 @@ public class LagTimeReportingController {
 			}
 		});
 		binder.registerCustomEditor(LagTimeReportSetup.class, new PropertyEditorSupport() {
-			
+
 			@Override
 			public void setAsText(String text) {
 				LagTimeReportSetup lagtime = lagtimeService.getLagTimeReportSetup(Integer.parseInt(text));
 				super.setValue(lagtime);
 			}
 		});
+		
+		binder.registerCustomEditor(Encounter.class, new PropertyEditorSupport() {
+
+			@Override
+			public void setAsText(String text) {
+				Encounter encounter = encounterService.getEncounter(Integer.parseInt(text));
+				super.setValue(encounter);
+			}
+		});
 	}
-	
+
 	@RequestMapping(value = "/module/lagtimereport/lagtimeReporting", method = RequestMethod.GET)
 	public void showForm() {
-		
+
 	}
-	
+
 	@ModelAttribute("printReport")
 	public LagTimeReport getLagTimeReport(@RequestParam(value = "reportId", required = false) Integer reportId) {
-		
 		LagTimeReport lagTimeReport = reportService.getLagTimeReport(reportId);
-		log.error(lagTimeReport.getId());
-		log.error(lagTimeReport.getLagtimereportSetup().getName());
-		log.error(lagTimeReport.getLagtimereportSetup().getForms());
-		
 		return lagTimeReport;
 	}
-	
+
+	@ModelAttribute("printEncounter")
+	public Object getLagTimeReporting(@RequestParam(value = "reportId", required = false) Integer reportId) {
+
+		LagTimeReport lagTimeReport = reportService.getLagTimeReport(reportId);
+		
+		List<Encounter> encounters = encounterService.getEncounters(null, null, lagTimeReport.getStartDate(),
+				lagTimeReport.getEndDate(), lagTimeReport.getLagtimereportSetup().getForms(), null, null, null, null,
+				false);
+
+		return encounters;
+	}
+
 	@ModelAttribute("printReports")
 	public List<LagTimeReport> getAllLagTimeReports() {
 		List<LagTimeReport> lagTimeReports = reportService.getAllLagTimeReports(false);
 		return lagTimeReports;
 	}
-	
-	//download PDF
+
+	// download PDF
 	@RequestMapping(value = "/module/lagtimereport/lagtimeReporting", method = RequestMethod.POST)
 	public String retireLagTimeReportSetup(HttpServletRequest request, ModelMap model) {
 		String reason = request.getParameter("reason");
@@ -111,5 +140,5 @@ public class LagTimeReportingController {
 		}
 		return "redirect:lagtimeReporting.list";
 	}
-	
+
 }
--- omod/src/main/webapp/lagtimeReporting.jsp
+++ omod/src/main/webapp/lagtimeReporting.jsp
@@ -6,6 +6,19 @@
 
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 
+<openmrs:htmlInclude
+	file="${pageContext.request.contextPath}/moduleResources/lagtimereport/js/jquery-3.1.1.min.js" />
+<openmrs:htmlInclude
+	file="${pageContext.request.contextPath}/moduleResources/lagtimereport/css/bootstrap.min.css" />
+<openmrs:htmlInclude
+	file="${pageContext.request.contextPath}/moduleResources/lagtimereport/js/bootstrap.min.js" />
+<openmrs:htmlInclude
+	file="${pageContext.request.contextPath}/moduleResources/lagtimereport/css/jquery.dataTables.min.css" />
+<openmrs:htmlInclude
+	file="${pageContext.request.contextPath}/moduleResources/lagtimereport/js/jquery.dataTables.min.js" />
+<openmrs:htmlInclude
+	file="${pageContext.request.contextPath}/moduleResources/lagtimereport/js/dataTable.js" />
+
 <div class="widget">
 	<h2>
 		<b class="boxHeader"><spring:message code="lagtimereport.reporting.title" /> ${printReport.reportId}</b>
@@ -36,6 +49,32 @@
 				<td><fmt:formatDate value="${printReport.startDate}" /> to <fmt:formatDate value="${printReport.endDate}" /></td>
 			</tr>
 		</table>
+		</br>
+		<table class="table table-striped">
+				<thead>
+					<tr>
+						<th><spring:message code="lagtimereport.reporting.form" /></th>
+						<th><spring:message code="lagtimereport.reporting.total.encounters.paper" /></th>
+						<th><spring:message code="lagtimereport.reporting.total.encouter.system" /></th>
+						<th><spring:message code="lagtimereport.reporting.percentage.entered" /></th>
+						<th><spring:message code="lagtimereport.reporting.average.lagtimes" /></th>
+						<th><spring:message code="lagtimereport.reporting.median.lagtimes" /></th>
+					</tr>
+				</thead>
+
+				<c:forEach var="report" items="${printReport.formValue}">
+					<tr>
+						<td>A</td>
+						<td>${report.numberOfForm}</td>
+						<td>A</td>
+						<td>A</td>
+						<td>A</td>
+						<td>A</td>
+					</tr>
+				</c:forEach>
+			</table>
+			<input type="submit"
+				value="<openmrs:message code="lagtimereport.reporting.exit"/>" name="exit" id="exit"/>
 	</div>
 		
 </div>
